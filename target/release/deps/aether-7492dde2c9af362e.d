9b141a8cc7203a70fe39fd0c910619a63c554a33
grulgalus <nee017310@gmail.com>
2026-08-16 09:21:51 +0200

    feat: Cílová distribuce přes .deb balíček a instalace do systému

---
+++ target/release/deps/aether-7492dde2c9af362e.d
@@ -0,0 +1,8 @@
+/data/data/com.termux/files/home/aether-lang/target/release/deps/aether-7492dde2c9af362e.d: src/main.rs src/lexer.rs src/ast.rs src/parser.rs
+
+/data/data/com.termux/files/home/aether-lang/target/release/deps/aether-7492dde2c9af362e: src/main.rs src/lexer.rs src/ast.rs src/parser.rs
+
+src/main.rs:
+src/lexer.rs:
+src/ast.rs:
+src/parser.rs:
