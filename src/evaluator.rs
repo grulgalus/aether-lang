7b92f1f5b783d3f7265428d330985e98b066a613
grulgalus <nee017310@gmail.com>
2026-08-17 16:53:47 +0200

    feat: v1.1 - Přidán nativní Web Server do Aetheru (funkce serve)

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::net::TcpListener;
+use std::io::{Read, Write};
 use crate::ast::{Program, Stmt, Expr};
 
 #[derive(Debug, Clone, PartialEq)]
@@ -23,26 +25,15 @@ impl Environment {
     pub fn get(&self, name: &str) -> Option<Object> { self.store.get(name).cloned() }
 }
 
-#[allow(dead_code)]
-fn check_program(_program: &Program) -> Result<(), String> { Ok(()) }
-
-#[allow(dead_code)]
-fn optimize_expr(expr: Expr) -> Expr { expr }
+#[allow(dead_code)] fn check_program(_program: &Program) -> Result<(), String> { Ok(()) }
+#[allow(dead_code)] fn optimize_expr(expr: Expr) -> Expr { expr }
 
 pub fn eval_program(program: &Program, env: &mut Environment) -> Object {
-    if let Err(e) = check_program(program) {
-        let err = Object::Error(format!("Chyba analýzy: {}", e));
-        env.output.push(err.to_string_val());
-        return err;
-    }
+    if let Err(e) = check_program(program) { return Object::Error(format!("Chyba analýzy: {}", e)); }
     let mut result = Object::Null;
     for stmt in &program.statements {
         result = eval_statement(stmt, env);
-        if let Object::Error(e) = result {
-            let msg = format!("🛑 KRITICKÁ CHYBA: {}", e);
-            env.output.push(msg);
-            return Object::Error(e);
-        }
+        if let Object::Error(e) = result { return Object::Error(e); }
     }
     result
 }
@@ -56,37 +47,12 @@ fn eval_block(stmts: &[Stmt], env: &mut Environment) -> Object {
 fn eval_statement(stmt: &Stmt, env: &mut Environment) -> Object {
     match stmt {
         Stmt::Let { name, value } | Stmt::Assign { name, value } => { let val = eval_expression(value, env); if let Object::Error(_) = val { return val; } env.set(name.clone(), val); Object::Null }
-        
-        // OPRAVA PAMĚTI (Přidáno .clone())
-        Stmt::Print { value } => { 
-            let val = eval_expression(value, env); 
-            if let Object::Error(_) = val { return val; } 
-            let text = val.to_string_val(); 
-            env.output.push(text.clone()); 
-            println!("{}", text); 
-            Object::Null 
-        }
-        
-        Stmt::If { condition, consequence, alternative } => { 
-            let cond = eval_expression(condition, env); if let Object::Error(_) = cond { return cond; }
-            let is_truthy = match cond { Object::Boolean(b) => b, Object::Number(n) => n != 0.0, Object::Null => false, _ => true }; 
-            if is_truthy { eval_block(consequence, env) } else if let Some(alt) = alternative { eval_block(alt, env) } else { Object::Null } 
-        }
-        Stmt::While { condition, body } => { 
-            let mut res = Object::Null; 
-            loop { 
-                let cond = eval_expression(condition, env); if let Object::Error(_) = cond { return cond; }
-                let is_truthy = match cond { Object::Boolean(b) => b, Object::Number(n) => n != 0.0, Object::Null => false, _ => true }; 
-                if !is_truthy { break; } 
-                res = eval_block(body, env); if let Object::Error(_) = res { return res; }
-            } 
-            res 
-        }
+        Stmt::Print { value } => { let val = eval_expression(value, env); if let Object::Error(_) = val { return val; } let text = val.to_string_val(); env.output.push(text.clone()); println!("{}", text); Object::Null }
+        Stmt::If { condition, consequence, alternative } => { let cond = eval_expression(condition, env); if let Object::Error(_) = cond { return cond; } let is_truthy = match cond { Object::Boolean(b) => b, Object::Number(n) => n != 0.0, Object::Null => false, _ => true }; if is_truthy { eval_block(consequence, env) } else if let Some(alt) = alternative { eval_block(alt, env) } else { Object::Null } }
+        Stmt::While { condition, body } => { let mut res = Object::Null; loop { let cond = eval_expression(condition, env); if let Object::Error(_) = cond { return cond; } let is_truthy = match cond { Object::Boolean(b) => b, Object::Number(n) => n != 0.0, Object::Null => false, _ => true }; if !is_truthy { break; } res = eval_block(body, env); if let Object::Error(_) = res { return res; } } res }
         Stmt::For { variable, iterable, body } => { let iter_val = eval_expression(iterable, env); if let Object::Array(arr) = iter_val { for item in arr { env.set(variable.clone(), item); let res = eval_block(body, env); if let Object::Error(_) = res { return res; } } Object::Null } else { Object::Error("For vyžaduje pole".into()) } }
         Stmt::Expression(expr) => eval_expression(expr, env),
         Stmt::Return { value } => eval_expression(value, env),
-        
-        // Smazali jsme _ => Object::Null, protože jsme pokryli všechny možné varianty Stmt!
         Stmt::Function { .. } | Stmt::Actor { .. } | Stmt::Import(_) => Object::Null,
     }
 }
@@ -99,13 +65,7 @@ fn eval_expression(expr: &Expr, env: &Environment) -> Object {
         Expr::Identifier(s) => env.get(s).unwrap_or(Object::Error(format!("Neznámá proměnná '{}'", s))),
         Expr::Array(els) => { let mut arr = Vec::new(); for el in els { let e = eval_expression(el, env); if let Object::Error(_) = e { return e; } arr.push(e); } Object::Array(arr) }
         Expr::Dict(pairs) => { let mut map = HashMap::new(); for (k, v) in pairs { let val = eval_expression(v, env); if let Object::Error(_) = val { return val; } map.insert(k.clone(), val); } Object::Dict(map) }
-        Expr::Index { left, index } => { 
-            let l = eval_expression(left, env); if let Object::Error(_) = l { return l; }
-            let i = eval_expression(index, env); if let Object::Error(_) = i { return i; }
-            if let (Object::Array(arr), Object::Number(idx)) = (&l, &i) { let index_usize = *idx as usize; if index_usize < arr.len() { return arr[index_usize].clone(); } else { return Object::Error("Index je mimo pole".into()); } }
-            if let (Object::Dict(map), Object::StringObj(k)) = (&l, &i) { if let Some(val) = map.get(k) { return val.clone(); } else { return Object::Error("Klíč neexistuje".into()); } }
-            Object::Error("Lze indexovat jen pole a dict".into())
-        }
+        Expr::Index { left, index } => { let l = eval_expression(left, env); if let Object::Error(_) = l { return l; } let i = eval_expression(index, env); if let Object::Error(_) = i { return i; } if let (Object::Array(arr), Object::Number(idx)) = (&l, &i) { let index_usize = *idx as usize; if index_usize < arr.len() { return arr[index_usize].clone(); } else { return Object::Error("Index je mimo pole".into()); } } if let (Object::Dict(map), Object::StringObj(k)) = (&l, &i) { if let Some(val) = map.get(k) { return val.clone(); } else { return Object::Error("Klíč neexistuje".into()); } } Object::Error("Lze indexovat jen pole a dict".into()) }
         Expr::Call { function, args } => {
             let mut eval_args = Vec::new(); 
             for arg in args { let e = eval_expression(arg, env); if let Object::Error(_) = e { return e; } eval_args.push(e); }
@@ -115,25 +75,35 @@ fn eval_expression(expr: &Expr, env: &Environment) -> Object {
                 "upper" => { if let Some(Object::StringObj(s)) = eval_args.get(0) { return Object::StringObj(s.to_uppercase()); } Object::Error("upper() vyžaduje text".to_string()) },
                 "lower" => { if let Some(Object::StringObj(s)) = eval_args.get(0) { return Object::StringObj(s.to_lowercase()); } Object::Error("lower() vyžaduje text".to_string()) },
                 "str" => { if let Some(obj) = eval_args.get(0) { return Object::StringObj(obj.to_string_val()); } Object::Null },
+                
+                // 🌐 TADY JE NOVÁ MAGIE: AETHER WEB SERVER!
+                "serve" => {
+                    if let (Some(Object::Number(port)), Some(Object::StringObj(html))) = (eval_args.get(0), eval_args.get(1)) {
+                        let addr = format!("0.0.0.1:{}", *port as u16);
+                        println!("🌐 Aether Server běží na http://127.0.0.1:{}", *port as u16);
+                        if let Ok(listener) = TcpListener::bind(&addr) {
+                            for stream in listener.incoming() {
+                                if let Ok(mut stream) = stream {
+                                    let mut buffer = [0; 512];
+                                    let _ = stream.read(&mut buffer);
+                                    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n{}", html);
+                                    let _ = stream.write_all(response.as_bytes());
+                                }
+                            }
+                        } else { return Object::Error(format!("Port {} je už obsazený!", port)); }
+                        return Object::Null;
+                    }
+                    Object::Error("serve() vyžaduje (číslo_portu, text_stránky)".into())
+                },
                 _ => Object::Error(format!("Neznámá funkce '{}'", function))
             }
         }
         Expr::BinaryOp { left, operator, right } => {
             let l = eval_expression(left, env); if let Object::Error(_) = l { return l; }
             let r = eval_expression(right, env); if let Object::Error(_) = r { return r; }
-            
             if operator == "&&" { let is_l = match &l { Object::Boolean(b) => *b, Object::Number(n) => *n != 0.0, _ => true }; let is_r = match &r { Object::Boolean(b) => *b, Object::Number(n) => *n != 0.0, _ => true }; return Object::Boolean(is_l && is_r); }
             if operator == "||" { let is_l = match &l { Object::Boolean(b) => *b, Object::Number(n) => *n != 0.0, _ => true }; let is_r = match &r { Object::Boolean(b) => *b, Object::Number(n) => *n != 0.0, _ => true }; return Object::Boolean(is_l || is_r); }
-            
-            if let (Object::Number(v1), Object::Number(v2)) = (&l, &r) {
-                match operator.as_str() {
-                    "+" => Object::Number(v1 + v2), "-" => Object::Number(v1 - v2), "*" => Object::Number(v1 * v2), "/" => if *v2 != 0.0 { Object::Number(v1 / v2) } else { Object::Error("Dělení nulou".into()) },
-                    "<" => Object::Boolean(v1 < v2), ">" => Object::Boolean(v1 > v2), "==" => Object::Boolean(v1 == v2), "!=" => Object::Boolean(v1 != v2), "<=" => Object::Boolean(v1 <= v2), ">=" => Object::Boolean(v1 >= v2),
-                    _ => Object::Error("Neznámý operátor".into())
-                }
-            } else if let (Object::StringObj(s1), Object::StringObj(s2)) = (&l, &r) {
-                if operator == "+" { Object::StringObj(format!("{}{}", s1, s2)) } else if operator == "==" { Object::Boolean(s1 == s2) } else if operator == "!=" { Object::Boolean(s1 != s2) } else { Object::Error("Na text nelze použít tento operátor".into()) }
-            } else { Object::Error("Matematika vyžaduje čísla nebo spojování textu".into()) }
+            if let (Object::Number(v1), Object::Number(v2)) = (&l, &r) { match operator.as_str() { "+" => Object::Number(v1 + v2), "-" => Object::Number(v1 - v2), "*" => Object::Number(v1 * v2), "/" => if *v2 != 0.0 { Object::Number(v1 / v2) } else { Object::Error("Dělení nulou".into()) }, "<" => Object::Boolean(v1 < v2), ">" => Object::Boolean(v1 > v2), "==" => Object::Boolean(v1 == v2), "!=" => Object::Boolean(v1 != v2), "<=" => Object::Boolean(v1 <= v2), ">=" => Object::Boolean(v1 >= v2), _ => Object::Error("Neznámý operátor".into()) } } else if let (Object::StringObj(s1), Object::StringObj(s2)) = (&l, &r) { if operator == "+" { Object::StringObj(format!("{}{}", s1, s2)) } else if operator == "==" { Object::Boolean(s1 == s2) } else if operator == "!=" { Object::Boolean(s1 != s2) } else { Object::Error("Na text nelze použít tento operátor".into()) } } else { Object::Error("Matematika vyžaduje čísla nebo spojování textu".into()) }
         }
     }
 }
